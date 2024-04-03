eef651feb2f006eeb20b27b8cba50e07af2d2bec
David Jones <davidthomjones@gmail.com>
2024-04-03 16:25:17 +1000

    Update canvasApiData.ts

--- src/lib/canvasApiData.ts
+++ src/lib/canvasApiData.ts
@@ -108,6 +108,158 @@ query cljBaseQuery {
   }`
 
 
+interface memberNode {
+  _id: string
+  createdAt: string
+  user: {
+    "_id": string
+  }
+}
+
+interface prompt {
+  // currently Canvas API REST response for view all topic
+  // @todo 
+  // - add some analysis etc.
+  // - maybe add additional data 
+  unread_entries: []
+  forced_entries: []
+  entry_ratings: {}
+  participants: []
+  view: []
+  new_entries: []
+}
+
+interface group {
+  _id: string
+  name: string
+  updatedAt: string
+  membersCount: number
+  canMessage: boolean
+  createdAt: string
+  membersConnection: {
+    nodes: memberNode[]
+  }
+  members: memberNode[]
+  prompts: {
+    [key: string]: prompt
+  }
+}
+
+/*enum discussion_type: { 
+    threaded,  // fully threaded
+    side_comment  // only one level of nested comments
+    }
+
+enum read_state {
+    read,
+    unread
+}*/
+
+interface author {
+  id: number
+  anonymous_id: string
+  display_name: string
+  avatar_image_url: string
+  html_url: string
+  pronouns: string
+}
+
+interface group_topic_children {
+  id: number
+  group_id: number
+}
+
+interface discussionTopics {
+  id: number
+  title: string
+  last_reply_at: string
+  created_at: string
+  delayed_post_at: string
+  assignment_id: number
+  root_topic_id: number
+  position: number
+  podcast_has_student_posts: boolean
+  discussion_type: string // values threaded or side_comment
+  lock_at: string
+  allow_rating: boolean
+  only_graders_can_rate: boolean
+  sort_by_rating: boolean
+  is_section_specific: boolean
+  anonymous_state: string
+  user_name: string
+  discussion_subentry_count: number
+  permissions: {
+    attach: boolean
+    update: boolean
+    reply: boolean
+    delete: boolean
+  }
+  require_initial_post: boolean
+  user_can_see_posts: boolean
+  podcast_url: string
+  read_state: string // read or unread 
+  unread_count: number
+  subscribed: boolean
+  attachments: []
+  published: boolean
+  can_unpublish: boolean
+  locked: boolean
+  can_lock: boolean
+  comments_disabled: boolean
+  author: author
+  html_url: string
+  url: string
+  pinned: boolean
+  group_category_id: number
+  can_group: boolean
+  topic_children: number[]
+  group_topic_children: group_topic_children[]
+  locked_for_user: boolean
+  message: string
+  subscription_hold: string
+  todo_date: string
+  is_announcement: boolean
+}
+
+/*interface learningJournalStatus {
+  privateJournal: boolean
+  completedConfig: boolean
+  promptsCreated: boolean
+  groupsCreated: boolean
+  selfSignUp: boolean
+  studentsWithoutGroup: boolean
+  multiStudentGroups: boolean
+}*/
+
+interface groupSet {
+  // Canvas API GraphQL
+  id: string
+  _id: string
+  name: string
+  memberLimit: string
+  selfSignup: string
+  groupsConnection: { "nodes": group[] }
+  groups: group[]
+  numGroups: number
+  numNonPrivateGroups: number
+  numStudentsMembersOfGroups: number
+  numStudents: number
+  discussionTopics: discussionTopics[]
+  numPrompts: number
+  learningJournalStatus: learningJournalStatus
+  groupsById: { [key: string]: group }
+  discussionTopicsById: { [key: string]: discussionTopics }
+}
+
+interface user {
+  _id: string
+  name: string
+  email: string
+  avatarUrl: string
+}
+
+
+
 let instance: any;
 
 export class learningJournalStatus {
@@ -117,6 +269,7 @@ export class learningJournalStatus {
   public groupsCreated: boolean = false; // there are groups
   public selfSignUp: boolean = false; // self-signup is enabled
   public studentsWithoutGroup: boolean = false; // there are students without a group
+  public multiStudentGroups: boolean = false; // there are groups with more than one student
 
   /**
    * @constructor
@@ -148,19 +301,29 @@ class canvasApiData {
   public id: number;  // Canvas course id
   public name: string; // Canvas course name
   public hostName: string = ''; // The hostname of the Canvas instance
-  public updated: number = 0;
+  public updated: number = 0 // TODO consider moving to a struct to include percent loaded etc
 
   public courseCode: string = ''; // The course code
   public createdAt: string = ''; // The creation date of the course
-  public groupSets: Object[] = []; // array of group sets in the course
-  public groupSetsById: any = {}; // object of group sets by id
+  //public groupSets: Object[] = []; // array of group sets in the course
+  public groupSets: groupSet[] = []
+  public groupSetsById: { [key: string]: groupSet } = {}
+  //public groupSetsById: any = {}; // object of group sets by id
 
-  public students: Object[] = []; // array of students in the course
-  public teachers: Object[] = []; // array of teachers in the course
-  public studentsById: any = {}; // object of students by id
-  public teachersById: any = {}; // object of teachers by id
-  public discussionTopics: any = {}
+  //public students: Object[] = []; // array of students in the course
+  //public teachers: Object[] = []; // array of teachers in the course
+  public students: user[] = []
+  public teachers: user[] = []
+  public studentsById: { [key: string]: user } = {}
+  public teachersById: { [key: string]: user } = {}
+  public discussionTopics: discussionTopics[] = []
+  //public learningJournalStatus: learningJournalStatus
   public learningJournalStatus: learningJournalStatus = new learningJournalStatus({})
+  /*public studentsById: any = {}; // object of students by id
+  public teachersById: any = {}; // object of teachers by id
+  public discussionTopics: any = {} */
+
+
 
   /**
    * @constructor
@@ -300,7 +463,7 @@ class canvasApiData {
       if (groupSet.selfSignup === "disabled") {
         groupSet.selfSignup = "X";
       }
-      let studentMemberIds = {}
+      let studentMemberIds : { [key: string]: boolean } = {}
       groupSet.numNonPrivateGroups = 0
       for (const group of groupSet.groups) {
         // copy group.membersConnection.nodes to group.members
@@ -379,7 +542,8 @@ class canvasApiData {
         // temporary call here...@todo move it some where sle
         this.getGroupsResponses();
 
-        this.updated += 1
+        // set this now and the root component will start working
+        //this.updated += 1
         if (DEBUG && GLOBAL_DEBUG) {
           console.log(`${FILE_NAME} :retrieveDiscussionTopics: got ALLLL data`)
           console.log(this)
@@ -443,30 +607,74 @@ class canvasApiData {
   /**
    * @method getGroupsResponses
    * @description Retrieve all the responses for all the groups in all of the
-   * group sets 
+   * group sets.
    * 
-   * - for each group set (this.groupSets array)
-   *    - for each group in the group set 
-   *       - get the group's prompt and responses 
-   *          Each prompt is a topic, so pass in the list of topic ids
-   *  
+   * Adds a prompts property to each group object. Keyed on the discussion topic Ids 
+   * for the group set and containing the Canvas API full topic view for the group's
+   * version of the topic.
+   * 
+   * TODO
+   * - Perhaps should include some stats/analysis extra stuff at this stage
+   * - The groupset should have properties to handle the loading of all the responses
+   *    for showing progress
+   * 
+   * For each groupset, proceed only if there are discussionTopics
+   *  For each group in the groupset
+   *    Find each discussion topic find the list of ids for all the group's specific topics
+   *    For each of the group's specific topics
+   *      Get a Canvas API full topic view
+   *      add it to the group.prompts property keyed on the groupset discussion topic
+   * 
    * @todo 
    * - maybe add in assignments
    *   
    *   
    */
 
-  async getGroupsResponses() {
+  async getGroupsResponses(): Promise<void> {
+    // to be used with some progress thing
+    const numApiCalls = this.calculateGroupsResponseCalls()
+    if (DEBUG && GLOBAL_DEBUG) {
+      console.log(`${FILE_NAME} getGroupsResponses: numApiCalls: ${numApiCalls}`)
+    }
+
     for (const groupSet of this.groupSets) {
+      if (!groupSet.discussionTopics) {
+        // TODO why does this happen??
+        continue
+      }
       for (const group of groupSet.groups) {
         // get discussion topic ids for the group from keys of discussionTopicsById 
-        const topicIds = Object.keys(groupSet.discussionTopicsById)
-        for (const topicId of topicIds) {
-          let data = await this.getResponses( topicId)
+        group.prompts = {}
+        let groupTopics = []
+        // convert group._id to integer
+        const groupId = parseInt(group._id)
+        // TODO what is the topic ID and API call I should be using to get
+        // a group's topic ??? *************
+        for (const topic of groupSet.discussionTopics) {
+          // each of groupSet.discussionTopics elements contain 
+          //  group_topic_children property - an array of topic_id and group_id
+          // extract the topic_id that matches the current group's group_id
+          let groupTopic = topic.group_topic_children.find((element: any) => element.group_id === groupId)
+          groupTopics.push({
+            topicId: topic.id, // link back to parent topic
+            groupId: groupId,
+            groupTopicId: groupTopic.id // the group's topic
+          })
+        }
+        if (DEBUG && GLOBAL_DEBUG) {
+          console.log(`${FILE_NAME} getGroupsResponses: groupTopicIds`)
+          console.log(groupTopics)
+        }
+        for (const groupTopic of groupTopics) {
+          // data is the full topic view for one groups prompt
+          let data = await this.getResponses(group._id, groupTopic.groupTopicId)
           console.log(`groupGroupsResponses`)
           console.log(data)
+          group.prompts[groupTopic.topicId] = data
+          //group.prompts.push(data)
           if (data) {
-            // @TODO where does htis get added
+            // @TODO where does this get added
             // add the responses to the group object
           }
         }
@@ -476,57 +684,75 @@ class canvasApiData {
       console.log(`${FILE_NAME} getGroupsResponses: got all responses`)
       console.log(this)
     }
-  } 
+    this.updated +=1
+  }
+
+  /**
+   * @method calculateGroupsResponseCalls
+   * @description Calculate the number of API calls required to 
+   *    get all the responses for all the groups
+   * @returns The number of API calls required
+   */
+
+  calculateGroupsResponseCalls(): number {
+    let numApiCalls = 0
+    for (const groupSet of this.groupSets) {
+      numApiCalls += groupSet.numGroups * groupSet.numPrompts
+    }
+    return numApiCalls
+  }
 
   /**
    * @method getResponses
+   * @param groupId
    * @param topicId 
    * @description  for the given topic get the full topic view from Canvas API
    * GET /api/v1/courses/:course_id/discussion_topics/:topic_id/view 
    */
 
-  async getResponses( topicId : string) : any {
+  async getResponses(groupId: string, topicId: string): Promise<any> {
     const courseId = this.id.toString()
-    const callUrl = `${this.hostName}/api/v1/courses/${courseId}/discussion_topics/${topicId}/view`;
+    //const callUrl = `${this.hostName}/api/v1/courses/${courseId}/discussion_topics/${topicId}/view`;
+    const callUrl = `${this.hostName}/api/v1/groups/${groupId}/discussion_topics/${topicId}/view`
 
     if (DEBUG && GLOBAL_DEBUG) {
-      console.log(`${FILE_NAME} getResponses: Hostname: ${this.hostName}; Course ID: ${courseId}; Topic ID: ${topicId}`);
+      console.log(`${FILE_NAME} getResponses: Hostname: ${this.hostName}; Course ID: ${courseId}; Group Id: ${groupId} Topic ID: ${topicId}`);
     }
 
     try {
-        const response = await fetch(callUrl, {
-            method: "GET",
-            credentials: "include",
-            headers: {
-                "Content-Type": "application/json",
-                Accept: "application/json",
-            },
-        });
-        if (!response.ok) {
-            throw new Error(
-                `${FILE_NAME} getResponses: error ${response.status}`
-            );
-        }
+      const response = await fetch(callUrl, {
+        method: "GET",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+      });
+      if (!response.ok) {
+        throw new Error(
+          `${FILE_NAME} getResponses: error ${response.status}`
+        );
+      }
 
-        const data: any = await response.json();
-        if (DEBUG && GLOBAL_DEBUG) {
-            console.log(`${FILE_NAME} got some data`)
-            console.log(data)
-        }
-        if (!data) {
-            throw new Error(`${FILE_NAME}:getCourseObject: no data returned`);
-            return {}
-        }
+      const data: any = await response.json();
+      if (DEBUG && GLOBAL_DEBUG) {
+        console.log(`${FILE_NAME} got some data`)
+        console.log(data)
+      }
+      if (!data) {
+        throw new Error(`${FILE_NAME}:getCourseObject: no data returned`);
+        return {}
+      }
 
-//        if (includeModules) {
- //           const modules = await getCourseModules(hostName, courseId);
-  //          data.modules = modules;
-   //     } 
-        return data;
+      //        if (includeModules) {
+      //           const modules = await getCourseModules(hostName, courseId);
+      //          data.modules = modules;
+      //     } 
+      return data;
     } catch (error) {
-        console.error(`canvasApi:getCourseObject: error ${error}`);
-        throw error;
-    } 
+      console.error(`canvasApi:getCourseObject: error ${error}`);
+      throw error;
+    }
   }
 
 }
